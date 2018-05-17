3efb179bff3ba7235b678a9c83b473068ac1a713
William Reilly <wreilly2001@gmail.com>
2018-05-17 13:51:00 -0400

    Comments removal cleanup, mostly

--- client/src/app/article-list/article-list.component.ts
+++ client/src/app/article-list/article-list.component.ts
@@ -288,9 +288,11 @@ export class ArticleListComponent {
         // https://stackoverflow.com/questions/16104078/appending-array-to-formdata-and-send-via-ajax
         this.myFormFieldsData.append('articlePhotos_name', JSON.stringify(this.photosRenamedFilenamesArray)) // SAME as on Template Form (fwiw)
 
+/* Worked fine. See browser DevTools Network "Request Payload"
          var myxhr = new XMLHttpRequest;
          myxhr.open('POST', '/', true);
          myxhr.send(this.myFormFieldsData);
+*/
 
         return this.myFormFieldsData;
 
@@ -299,49 +301,16 @@ export class ArticleListComponent {
 
     public addArticle(addArticleFormTemplate_refPassedIn) {
 
-        /* N.B. From the TEMPLATE Form, I pass in the whole ngForm reference
-         on-ngSubmit="addArticle(addArticleFormTemplate_ref)"
-         */
-
-        console.log('addArticleFormTemplate_refPassedIn: WHOLE DURNED THING (TEMPLATE only) ', addArticleFormTemplate_refPassedIn);
-        /* Interesting:
-        NgForm { control: FormGroup, controls: { articleUrl_name_template: FormControl, ... } }
-        */
-
-        if (addArticleFormTemplate_refPassedIn) {
-            console.log('addArticleFormTemplate_refPassedIn: .VALUE (TEMPLATE only) ', addArticleFormTemplate_refPassedIn.value);
-            /* Yes:
-             {articleUrl_name: "http://nytimes.com", articleTitle_name: "TEMPLATE We Wrote This Yesterday. (So long as Today is Tomorrow.)"}
-             */
-        }
-        
-        console.log('we are in addArticle - this.addArticleForm.value (REACT only) ', this.addArticleForm.value);
-        /* Yeah. V. nice.
-         {articleUrl_formControlName: "http://nytimes.com", articleTitle_formControlName: "REACTIVE We Wrote This Yesterday. (So long as Today is Tomorrow.)"}
-         */
-
         let myFormFieldsAndFiles: any; // FormData & Etc.
 
         if (addArticleFormTemplate_refPassedIn) {
             // TEMPLATE-DRIVEN
-/*
-            articleToCreate = {
-                articleUrl_name: addArticleFormTemplate_refPassedIn.articleUrl_name,
-                articleTitle_name: addArticleFormTemplate_refPassedIn.articleTitle_name,
-            }
-*/
-
-            // ***********************************
+            console.log('addArticleFormTemplate_refPassedIn.value: (TEMPLATE) ', addArticleFormTemplate_refPassedIn.value);
                 myFormFieldsAndFiles  = this.prepareToAddArticleTemplateForm(addArticleFormTemplate_refPassedIn);
-            // ***********************************
-
-
         } else {
-             // ***********************************
+            console.log('we are in addArticle - this.addArticleForm.value (REACT only) ', this.addArticleForm.value);
              myFormFieldsAndFiles = this.prepareToAddArticleReactiveForm();
-            // ***********************************
         }
-
 
         this.loadingPhotos = true;
 
@@ -372,156 +341,37 @@ export class ArticleListComponent {
          ------WebKitFormBoundaryoPpanl3RULBL6o14--
          */
 
-
-        /*
-         if (addArticleFormTemplate_refPassedIn) {
-            // TEMPLATE-DRIVEN
-            articleToCreate = {
-                articleUrl_name: addArticleFormTemplate_refPassedIn.articleUrl_name,
-                articleTitle_name: addArticleFormTemplate_refPassedIn.articleTitle_name,
-                articlePhotos_name: this.photosFilenamesArray
-            };
-            console.log('TEMPLATE articleToCreate is ', articleToCreate) // Yes we have the pics
-        } else {
-            // REACTIVE-MODEL-DRIVEN
-            articleToCreate = {
-                articleUrl_name: this.addArticleForm.value.articleUrl_formControlName,
-                articleTitle_name: this.addArticleForm.value.articleTitle_formControlName,
-                articlePhotos_name: this.photosFilenamesArray
-            };
-
-            // WE ARE DOING REACTIVE ONLY (thus far)
-        }
-
-
-
-        /* 2018-04-22 "C" in CRUD time:
-
-         From here in the Component's TypeScript,
-         we invoke the Article Service,
-         to use its Angular HTTP
-         to hit the REST API endpoint:
-
-(akin to how we used Axios in the Express App for the CLIENT-SIDE JAVASCRIPT to talk to the REST API),
-
-           POST to http://0.0.0.0:8089/articles
-         */
-  //      console.log('-02- BOUT TO GO TO SOIVICE  myFormFieldsAndFiles ', myFormFieldsAndFiles) // // You cannot see FormData via console.log()
-
-
-
-
-        /*
-                this._myArticleService.createArticle(articleToCreate) // << "Worked" to send photo filenames, not files.
-        */
-        // WE ARE DOING REACTIVE ONLY (thus far)
         this._myArticleService.createArticle(myFormFieldsAndFiles)
             .subscribe(
                 (whatIJustCreated: any) => {
                     // Observable success
                     console.log('whatIJustCreated ', whatIJustCreated);
-                    // Type {}
                     this.articleIJustCreatedDisplay = whatIJustCreated;
                     this.articleIJustCreatedBoolean = true;
 
                     /*
-                    O la. JSON.stringify of Array going into MongoDB
-                    Time to JSON.parse what we get out. I think
+                    IN:  JSON.stringify of Array going into MongoDB
+                    OUT: Must do JSON.parse what we get out
                      */
-                    console.log(' $$$$$$$$$$$$$$$$$$$$   whatIJustCreated.articlePhotos ', whatIJustCreated.articlePhotos)
+
                     console.log('whatIJustCreated.articlePhotos JSON.parse() ', JSON.parse(whatIJustCreated.articlePhotos))
                     /*
-                    Damn. Looks like that worked. Hot damn.  20180510-1716
-
-                    Of course, if you really do go this JSON/STRINGIFY/PARSE route, well,
-                    you are stuck with a database that for EVERY TIME you obtain
-                    this info, you MUST do JSON PARSE to use it.
-                    That is, your database entry is really holding a STRING. ugh.
-                    Kinda ugly.
-                    O well.
-                    Good enough for tonight's assignment! !! !!!
-
-
                      whatIJustCreated.articlePhotos JSON.parse()
-                     (2) ["sometimes__1525986950360_010006-MexAmerican.jpg", "sometimes__1525986950363_AndToThinkWeAllPlayedASma…t-NewYorkerCartoon-SlackScreenshot-2017-11-14.jpg"]
-                     0
-                     :
-                     "sometimes__1525986950360_010006-MexAmerican.jpg"
-                     1
-                     :
-                     "sometimes__1525986950363_AndToThinkWeAllPlayedASmallPart-NewYorkerCartoon-SlackScreenshot-2017-11-14.jpg"
+
+                      [
+                        "sometimes__1525986950360_010006-MexAmerican.jpg",
+                        "sometimes__1525986950363_AndToThinkWeAllPlayedASma…t-NewYorkerCartoon-SlackScreenshot-2017-11-14.jpg"
+                      ]
                      */
 
-                    this.articleIJustCreatedDisplay.articlePhotos = JSON.parse(whatIJustCreated.articlePhotos) // whamma-jamma this reconstituted Array of strings onto that {} type. should go okay I expect.
+                    this.articleIJustCreatedDisplay.articlePhotos = JSON.parse(whatIJustCreated.articlePhotos);
 
-                    /* Working. Finally. Whoa. 20180510-1647. Due at 20180510-2359.
-
-                     whatIJustCreated
-                     {articlePhotos: Array(1), _id: "5af4adf4b0f4ef4a2394b7f8", articleUrl: "http://nytimes.com", articleTitle: "hy", __v: 0}
-                     articlePhotos
-                     :
-                     Array(1)
-                     0
-                     :
-                     "sometimes__1525984753235_010006-MexAmerican.jpg"
-                     length
-                     :
-                     1
-                     __proto__
-                     :
-                     Array(0)
-                     articleTitle
-                     :
-                     "hy"
-                     articleUrl
-                     :
-                     "http://nytimes.com"
-                     __v
-                     :
-                     0
-                     _id
-                     :
-                     "5af4adf4b0f4ef4a2394b7f8"
-                     */
-
-
-
-                    // New: Let's update the Articles array right here, right now.
-                    // Also: The user-selected "How Many Articles" array.
-                    // On create here, I do *not* refresh the page,
-                    //  because I wanted to immediately display the just-created article.
-                    //  Till now I had not thought to do this array-pushing.
-                    //  Instead, the user had to refresh or perhaps navigate back
-                    //  to this page, to see full list with newly added item.
-                    //  This is better.
                     this.articles.push(this.articleIJustCreatedDisplay)
                     if (this.articlesHowMany.length) {
-                        // If the user has NOT clicked on "First n" Articles,
-                        //  we'll skip adding the just-created Article to that
-                        //  (non-existent) list on the page.
-                        //
-                        // That is, only if the user *HAS* already asked for some number
-                        // of Articles to appear will we add the
-                        // just-created Article to that displayed list:
                         this.articlesHowMany.push(this.articleIJustCreatedDisplay)
-                        /*
-                         One tiny side-effect (totally benign, acceptable, and prob. desired):
-                         re: "articlesHowMany":
-                         - If user says "Display first 2 articles", and then adds one article...
-                         - The "First n" articles list of 2 will be joined by the new one, in the
-                         next spot (3rd spot)
-                         (whereas it of course belongs ultimately in last spot)
-                         - That of course goes away next time they click on asking for any other
-                         number of articles to be displayed.
-                         (The newly added one will correctly be in the last spot.)
-                         */
                     } else {
-                        // do nothing. User had *not* clicked on "Display First n".
-                        // We do not add the just-created Article to a sort of "non-list"
+                        // do nothing to this "First n" list. User had *not* clicked on "Display First n".
                     }
-
-                    // https://stackoverflow.com/questions/36655922/resetting-a-form-in-angular-2-after-submit
-                    // Hmm. Seems to say resetForm() should be available o well.
                     this.addArticleForm.reset();
                 }
             );
